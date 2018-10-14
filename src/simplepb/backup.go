34daa46fab198454662c484c6a6414aebc9ff51b
Miguel Salcedo <msalcedo92@gmail.com>
2018-10-14 04:56:52 +0000

    Add skeleton for backup prepare method.

--- src/simplepb/backup.go
+++ src/simplepb/backup.go
@@ -2,18 +2,22 @@ package simplepb
 
 import "container/heap"
 
-func (srv *PBServer) backupPrepare() {
+func (srv *PBServer) backupPrepare(arguments *PrepareArgs, reply *PrepareReply) {
 	srv.mu.Lock()
 	defer srv.mu.Unlock()
 
-	// Add the prepared operation to the min-heap for uncommitted ops
-	heap.Push(&srv.uncommittedOperations, arguments)
+	if arguments.Index < srv.commitIndex {
+		reply.Success = true
+	} else {
+		// Add the prepared operation to the min-heap for uncommitted ops
+		heap.Push(&srv.uncommittedOperations, arguments)
 
-	// If the next operation in the uncommitted ops queue is the next to be committed,
-	// then commit the operations until you reach an operation that is out of order
-	for nextArgs := srv.uncommittedOperations.Peek(); srv.isNextOperation(nextArgs); nextArgs = srv.uncommittedOperations.Peek() {
-		heap.Pop(&srv.uncommittedOperations)
-		srv.commitIndex++
+		// If the next operation in the uncommitted ops queue is the next to be committed,
+		// then commit the operations until you reach an operation that is out of order
+		for nextArgs := srv.uncommittedOperations.Peek(); srv.isNextOperation(nextArgs); nextArgs = srv.uncommittedOperations.Peek() {
+			heap.Pop(&srv.uncommittedOperations)
+			srv.commitIndex++
+		}
 	}
 }
 
