75053f0130c9fecd52bce8670ac78290bf59b636
Miguel Salcedo <msalcedo92@gmail.com>
2018-10-14 11:41:04 +0000

    extracted backupPrepareInOrder method to defer unlocking the mutex. Also, added some more documentation.

--- src/simplepb/backup.go
+++ src/simplepb/backup.go
@@ -10,12 +10,28 @@ import (
 // This case is alright since the master commits all previous operations if any future operations return true.
 // So, once we have all in order operations the next operation will eventually return true.
 func (srv *PBServer) backupPrepare(arguments *PrepareArgs, reply *PrepareReply) {
-	srv.mu.Lock()
-
 	log.Printf("Replica %v received operation %v in view %v from primary in view %v.", srv.me, arguments.Index, srv.currentView, arguments.View)
 
 	// Add the prepared operation to the min-heap for uncommitted ops
 	prepare := &Prepare{args: arguments, reply: reply, done: make(chan bool, 1)}
+
+	go srv.backupPrepareInOrder(prepare)
+
+	// wait until the requested operation is prepared
+	<-prepare.done
+	log.Printf("Replica %v prepared operation %v in view %v from primary in view %v.", srv.me, arguments.Index, srv.currentView, arguments.View)
+}
+
+// Prepares the next set of operatons that are in order from the current opIndex
+// Does nothing if the next operation is not in the PrepareQueue.
+// For example, if we had operations [2, 4, 5] and we just received a request to prepare #1,
+// We would process ops 1 and 2, but not 4 and 5. Once we receive the message to prepare 3,
+// only then would we prepare 3, 4, and 5 in order.
+// Notice that this may cause the primary to timeout waiting for a response, but that's okay.
+func (srv *PBServer) backupPrepareInOrder(prepare *Prepare) {
+	srv.mu.Lock()
+	defer srv.mu.Unlock()
+
 	heap.Push(&srv.uncommittedOperations, prepare)
 
 	// If the next operation in the uncommitted ops queue is the next to be committed,
@@ -31,17 +47,13 @@ func (srv *PBServer) backupPrepare(arguments *PrepareArgs, reply *PrepareReply) 
 			srv.commitIndex = message.args.PrimaryCommit
 		}
 
-		message.reply.Success = srv.currentView == arguments.View && srv.opIndex >= message.args.Index
+		message.reply.Success = srv.currentView == message.args.View && srv.opIndex >= message.args.Index
 		message.reply.View = srv.currentView
 
 		srv.log = append(srv.log, message.args.Entry)
 
 		message.done <- true
 	}
-
-	srv.mu.Unlock()
-	<-prepare.done
-	log.Printf("Replica %v prepared operation %v in view %v from primary in view %v.", srv.me, arguments.Index, srv.currentView, arguments.View)
 }
 
 func (srv *PBServer) isNextOperation(message *Prepare) bool {
